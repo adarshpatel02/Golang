fafe0c9b80571255d53601cdeabf88b003974e4d
adarshpatel02 <adarshpatel702@gmail.com>
2023-12-18 01:19:00 +0530

    Golang Management

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/gofr-dev/gofr-ZopSmart
+
+go 1.17
+
+require (
+    github.com/gofr-dev/gofr v1.0.0
+)
+
---
+++ handler.test.go
@@ -0,0 +1,19 @@
+package tests
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"Zopsmart/handlers"
+	"Zopsmart/models"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetCars(t *testing.T) {
+	// Implement unit tests for GetCars handler
+	// Use httptest.NewRecorder to simulate HTTP requests
+}
+
+// Add similar test functions for other handlers
